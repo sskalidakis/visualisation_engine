d1e9a5c5a102965f7bab09221cf887ed02fcc500
epu_temp_steve <sskalidakis@epu.ntua.gr>
2020-08-27 13:16:03 +0300

    committed jquery.js

--- visualiser/views.py
+++ visualiser/views.py
@@ -6,6 +6,7 @@ from django.apps import apps
 
 from django.http import HttpResponse
 
+from data_manager.views import create_heatmap_data
 from visualiser.fake_data.fake_data import FAKE_DATA, COLUMNCHART_DATA, BAR_RANGE_CHART_DATA, BAR_HEATMAP_DATA, \
     HEAT_MAP_DATA, SANKEYCHORD_DATA, THERMOMETER, HEAT_MAP_CHART_DATA, PARALLEL_COORDINATES_DATA, PIE_CHART_DATA, \
     RADAR_CHART_DATA, PARALLEL_COORDINATES_DATA_2, BAR_HEATMAP_DATA_2, BAR_RANGE_CHART_DATA_2, SANKEYCHORD_DATA_2, \
@@ -476,102 +477,6 @@ def get_response_heat_map(request):
         json_response = json.loads(request.body)
         print(json_response)
     return json_response
-
-
-def create_heatmap_data(dataset, row_categorisation_dataset, col_order, row_order, dataset_type):
-    final_data = []
-    ranges_data = []
-    if dataset_type == 'file':
-        final_data = heatmap_chart_data_from_file(dataset)
-
-    elif dataset_type == 'db':
-        try:
-            dataset = Dataset.objects.get(dataset_name=dataset)
-            data_table = apps.get_model(DATA_TABLES_APP, dataset.dataset_django_model)
-            data = data_table.objects.all()
-            variables = Variable.objects.filter(dataset_relation=dataset.id).order_by('id')
-        except Exception as e:
-            log.error('Dataset or corresponding variables not found in order to complete the 2d histogram.')
-            log.error(e)
-            return e, 400
-
-        # The order of the variables is decided to be like this: column, row, value.
-        try:
-            col_ordering = heatmap_ordering(col_order, variables, 0)
-            row_ordering = heatmap_ordering(row_order, variables, 1)
-            data = heatmap_ordering_method(col_ordering, data, row_ordering)
-        except Exception as e:
-            log.error('Error while ordering the columns or rows for the histogram 2d')
-            log.error(e)
-            return e, 400
-
-        final_data = reformat_heatmap_data(data, variables)
-        # If guides/ranges are used the dataset of the guides has to be declared explicitly in the request
-        # TODO: We may need to change that (not sure how though)
-        # TODO: Also we may need to include column categorisation
-        ranges_data = heatmap_row_categorisation(row_categorisation_dataset)
-
-    return final_data, ranges_data
-
-
-def heatmap_row_categorisation(row_categorisation_dataset):
-    ranges_data = []
-    if row_categorisation_dataset != '':
-        row_ranges_table = apps.get_model(DATA_TABLES_APP, row_categorisation_dataset).objects.all()
-        for el in row_ranges_table:
-            dict_el = {'guide_from': el.guide_from, 'guide_to': el.guide_to, 'value': el.value}
-            ranges_data.append(dict_el)
-    return ranges_data
-
-
-def heatmap_chart_data_from_file(dataset):
-    final_data = []
-    with open('static/harmonisation_data/' + dataset, 'r') as f:
-        data = f.read()
-    diction = json.loads(data)
-    for model, vars in diction.items():
-        for var, val in vars.items():
-            final_data.append({"model": model, "variable": var, "value": val})
-    print(final_data)
-    return final_data
-
-
-def reformat_heatmap_data(data, variables):
-    final_data = []
-    for el in data:
-        dictionary = {}
-        for var in variables:
-            if var.variable_table_name is None:
-                dictionary[var.var_name] = getattr(el, var.var_name)
-            else:
-                var_table = apps.get_model(DATA_TABLES_APP, var.variable_table_name)
-                var_table_obj = var_table.objects.get(id=getattr(el, var.var_name).id)
-                value = var_table_obj.title
-                dictionary[var.var_name] = value
-        final_data.append(dictionary)
-    return final_data
-
-
-def heatmap_ordering_method(col_ordering, data, row_ordering):
-    if (col_ordering is None) and (row_ordering is None):
-        pass
-    elif col_ordering is None:
-        data = data.order_by(row_ordering)
-    elif row_ordering is None:
-        data = data.order_by(col_ordering)
-    else:
-        data = data.order_by(col_ordering, row_ordering)
-    return data
-
-
-def heatmap_ordering(order, variables, var_position):
-    ordering = None
-    django_model = apps.get_model(DATA_TABLES_APP, variables[var_position].variable_table_name)
-    fields = django_model._meta.get_fields()
-    for field in fields:
-        if order == field.name:
-            ordering = str(variables[var_position].var_name) + "__" + str(field.name)
-    return ordering
 
 
 @csrf_exempt
